49678d1a2a0985b36ff915b91150ed9a6e3d8296
Kyson Briggs <kysonjbriggs@gmail.com>
2021-09-13 13:31:07 -0600

    fixed syntax

--- project.js
+++ project.js
@@ -1,9 +1,9 @@
 module.exports = {
-  getGreeting(name) {
+  getGreeting: (name) => {
     return "Hi " + name;
   },
 
-  addN(num1, num2) {
+  addN: (num1, num2) => {
     return num1 + num2;
   },
 };
--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-const getGreeting = require('./project');
-const addN = require('./project')
+const { getGreeting, addN } = require('./project.js')
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
-});
+})
 
 test('can add numbers', () => {
   expect(addN(5,5)).toBe(10)
